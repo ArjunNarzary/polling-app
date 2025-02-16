382ff63ef076c7deaae48e59444640bf18d2d813
Arjun Narzary <arjunn@byteridge.com>
2025-02-16 16:12:45 +0530

    fix path issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../../prisma/generated/client"
 
 let prismaClient: PrismaClient
 
